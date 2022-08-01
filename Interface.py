ebead801ef4b1806157700e3ea10c376a0915251
Bara14 <bara14@op.pl>
2022-08-01 08:08:36 +0200

    Delete Interface.py

--- Interface.py
+++
@@ -1,6 +0,0 @@
-class InformalParserInterface:
-    def load_data_source(self, path: str, file_name: str) -> str:
-        pass
-
-    def extract_text(self, full_file_name: str) -> dict:
-        pass
