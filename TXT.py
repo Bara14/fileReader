db82408179ae12acc00aaeceb3890c98699a3869
Bara14 <bara14@op.pl>
2022-08-01 08:08:48 +0200

    Delete TXT.py

--- TXT.py
+++
@@ -1,7 +0,0 @@
-from Interface import InformalParserInterface
-class TXTReader(InformalParserInterface):
-    def load_data_source(self, file_name):
-        with open(file_name) as txtfile:
-            text = txtfile.read()
-            print(text)
-
