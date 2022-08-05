bf4e4c3f521011821c4bc7ee936046aec62d76dd
rkubik <bara14@op.pl>
2022-08-05 08:24:50 +0200

    fixes after  next refactor

--- Service/reader.py
+++ Service/reader.py
@@ -8,12 +8,16 @@ class Reader:
     def readFile(self, file):
         ext = mimetypes.guess_type(file)
         if ext[0] == "application/pdf":
+            print('I found PDF!!:')
             reader = PdfReader()
         elif ext[0] == "text/csv":
+            print('I found CSV!!:')
             reader = CsvReader()
         elif ext[0] == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            print('I found DOCX!!:')
             reader = DocxReader()
         elif ext[0] == "text/plain" or "application/vnd.oasis.opendocument.text":
+            print('I found TXT!!:')
             reader = TxtReader()
         return reader
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import mimetypes
 
 from Service.reader import Reader
+import os
+env_var = os.environ
+os.environ['FILE_HOME'] = '/home/rkubi/PycharmProjects/fileReader/Files/'
+pathToFolder = os.listdir(os.environ['FILE_HOME'])
 
-
-reader = Reader()
-file = reader.readFile('Files/name.pdf')
-file.load_data_source('Files/name.pdf')
-
+for file in pathToFolder:
+    reader = Reader()
+    read_object = reader.readFile(os.environ['FILE_HOME']+file)
+    read_object.load_data_source(os.environ['FILE_HOME']+file)
 
