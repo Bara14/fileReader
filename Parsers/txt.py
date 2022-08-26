666d08a55afc7a23954344eeb596b9f7d588d633
Bara14 <bara14@op.pl>
2022-08-26 09:38:37 +0200

    Delete Parsers directory

--- Parsers/txt.py
+++
@@ -1,9 +0,0 @@
-from Interface import InformalParserInterface
-
-
-class TxtReader(InformalParserInterface):
-    def load_data_source(self, file_name):
-        with open(file_name) as txtfile:
-            text = txtfile.read()
-            print(text)
-
